8aa6d5de4023c482a586a75ccd78c23a0f6fbc73
makskarv <makskarv@mail.ru>
2016-10-24 22:50:13 +0300

    work with db

--- controllers/MyController.php
+++ controllers/MyController.php
@@ -1,4 +1,4 @@
-﻿<?php
+<?php
 /**
  * Created by PhpStorm.
  * User: Валерий
@@ -15,7 +15,7 @@ class MyController extends Controller{
 
     public function actionIndex($id=null){
         $hi='Hello,World';
-        $names=['Ivanov','Petrov','Valera'];
+        $names=['Ivanov','Petrov','Sidorov'];
    if(!$id) $id='test';
         // return $this->render('index',['hello'=>$hi,'names'=>$names]);
         return $this->render('index',compact('hi','names','id'));
